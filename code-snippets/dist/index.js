cb564609d11caea38489f2f664afb6cdd60c998a
Supraja Aravamudhan <asupraja527@gmail.com>
2026-01-23 11:40:39 -0600

    Refactor TypeScript code: replace age logic with sales data, enhance function implementations, and add enums, arrays, and object examples

--- code-snippets/dist/index.js
+++ code-snippets/dist/index.js
@@ -1,10 +1,119 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let age = 42;
-if (age < 50) {
-    age = age + 10;
-    age = 50;
+let sales = 100000;
+let course = 'AI engineer';
+let success = true;
+let level;
+function get_doc(documents) {
+    console.log(documents);
 }
-console.log(age);
-console.log(age);
+console.log(sales, course, success, level);
+get_doc("Doc1");
+let nums = [];
+nums[0] = 2;
+nums[1] = 3;
+let user = [1, 'User1'];
+console.log(user[0]);
+var Sizes;
+(function (Sizes) {
+    Sizes[Sizes["Small"] = 1] = "Small";
+    Sizes[Sizes["Medium"] = 2] = "Medium";
+    Sizes[Sizes["Large"] = 3] = "Large";
+})(Sizes || (Sizes = {}));
+let mySize = Sizes.Medium;
+console.log(mySize);
+var Sizes2;
+(function (Sizes2) {
+    Sizes2[Sizes2["Small"] = 1] = "Small";
+    Sizes2[Sizes2["Medium"] = 2] = "Medium";
+    Sizes2[Sizes2["Large"] = 3] = "Large";
+})(Sizes2 || (Sizes2 = {}));
+let mySize2 = Sizes2.Medium;
+console.log(mySize2);
+function tax(sal, taxYear) {
+    if (sal > 12_000) {
+        return sal * 0.5;
+    }
+    if ((taxYear || 2022) == 1) {
+        return sal * 0.2;
+    }
+    return sal * 0.3;
+}
+console.log(tax(15_000, 1));
+console.log(tax(8_000));
+let employee = { id: 11, name: 'John' };
+console.log(employee.id);
+let employee2 = {
+    id: 12,
+    name: 'Mary',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+employee2.retire(new Date());
+let employee3 = {
+    id: 23, name: 'john', retire: (date) => { console.log(date); }
+};
+employee3.retire(new Date('03/23/2021'));
+function convert(weight) {
+    if (typeof weight === 'number') {
+        return weight;
+    }
+    else {
+        return (parseInt(weight));
+    }
+}
+convert('10kg');
+convert(10);
+let textBox = { drag: () => { },
+    resize: () => { } };
+console.log(textBox);
+let quantity;
+quantity = 50;
+let quantity2;
+quantity2 = '50';
+quantity2 = '100';
+let quantity3 = 100;
+quantity3 = 50;
+console.log(quantity3, quantity2, quantity);
+function greet(name) {
+    console.log(`Hello ,${name.toUpperCase()}!!`);
+}
+greet('John');
+function greet2(name) {
+    if (name) {
+        console.log(`Hello ,${name.toUpperCase()}!!`);
+    }
+    else {
+        console.log('Hello!!');
+    }
+}
+greet2(null);
+greet2(undefined);
+greet2('John');
+greet2('');
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let cust = getCustomer(0);
+if (cust !== null && cust !== undefined) {
+    console.log(cust.birthday);
+}
+console.log(cust?.birthday);
+console.log(cust?.birthday.getFullYear());
+let birthday = cust?.birthday.getFullYear() ?? 'No birthday available';
+console.log(birthday);
+let phone = document.getElementById('phone');
+phone.value = '123-456-7890';
+let phone2 = document.getElementById('phone2');
+phone2.value = '987-654-3210';
+function processEvents() {
+    while (true) {
+    }
+}
+let inferredString = 'This is a string';
+console.log(inferredString);
+let inferredNumber = 20;
+console.log(inferredNumber);
+processEvents();
 //# sourceMappingURL=index.js.map
--- code-snippets/src/index.ts
+++ code-snippets/src/index.ts
@@ -1,8 +1,207 @@
 
-let age: number = 42;
-if (age < 50 ) {
-    age= age + 10;
-    age = 50;
+let sales: number = 100000;
+let course: string = 'AI engineer';
+let success: boolean = true;
+let level;
+
+function get_doc(documents){ // any type, set the 'noImplicitAny' to false to allow this
+    console.log(documents)
 }
-console.log(age);
-console.log(age);
+
+console.log(sales,course,success,level)
+get_doc("Doc1")
+
+//------------------------------
+
+//Arrays
+
+let nums:number[] = []
+nums[0] = 2
+nums[1] = 3
+
+//Tuples
+//Keeps the types and order of elements fixed, limited number of elements to key value pairs
+let user:[number, string] = [1,'User1']
+console.log(user[0])
+
+
+//------------------------------
+//Enums
+
+enum Sizes { Small=1, Medium, Large }
+let mySize: Sizes = Sizes.Medium
+console.log(mySize) 
+
+const enum Sizes2 { Small=1, Medium, Large }
+let mySize2: Sizes2 = Sizes2.Medium
+console.log(mySize2) 
+
+//------------------------------
+//Functions 
+
+function tax(sal:number,taxYear?:number): number { // '?' makes the parameter optional
+//function tax(sal:number,taxYear:number=2022): number { // default value for taxYear is 2022
+    if (sal > 12_000) {
+        return sal*0.5;
+    }
+    if ((taxYear||2022) == 1) { // default value for taxYear is 2022 if not provided
+        return sal*0.2; 
+    }
+    return sal*0.3;
+}
+
+console.log(tax(15_000,1))
+console.log(tax(8_000)) // taxYear is optional
+
+//------------------------------
+//Objects
+
+let employee: { readonly id:number, name:string} = {id:11,name:'John'};
+
+console.log(employee.id)
+
+let employee2: { id:number, name:string, retire:(date:Date)=>void } = {
+    id:12,
+    name:'Mary',
+    retire: (date:Date) => { //Function as a property of an object
+        console.log(date)
+    }
+}
+employee2.retire(new Date())
+
+//problem with above approach is that we have to repeat the structure for every object
+//solution is to use 'type' or 'interface'
+
+//------------------------------
+//Advanced Types
+
+//1.Type Aliases or Custom Types
+//Using 'type' to define a custom type
+type Employee = { readonly id:number, name:string, retire:(date:Date)=> void } 
+
+let employee3:Employee = {
+    id:23,name:'john',retire: (date:Date) => { console.log(date); }
+}
+
+employee3.retire(new Date('03/23/2021'))
+
+
+//2.Union Types
+
+function convert(weight:string | number ): number { // weight can be string or number
+    if (typeof weight === 'number'){    //narrowing: check the type of weight
+        return weight;
+    }
+    else{
+        return (parseInt(weight))
+    }
+}
+convert('10kg')
+convert(10)
+
+//3.Intersection Types
+
+type Draggable = { drag: () => void};
+
+type Resizable = { resize: () => void};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = { drag: ()=>{},
+                          resize: ()=>{} };
+
+console.log(textBox); 
+
+//4.Literal Types
+
+let quantity: 50; // quantity can only be 50
+quantity = 50;
+//quantity = 100; // Error
+
+let quantity2: '50' | '100'; // quantity2 can be either '50' or '100'
+quantity2 = '50';
+quantity2 = '100';
+//quantity2 = '200'; // Error
+
+type Quantity = 50 | 100;
+let quantity3: Quantity = 100;
+quantity3 = 50;
+//quantity3 = 200; // Error
+console.log(quantity3, quantity2, quantity);
+//5.Nullable Types
+
+function greet(name:string): void {
+    console.log(`Hello ,${name.toUpperCase()}!!`)
+}
+// greet(null); // Error
+greet('John');
+
+function greet2(name:string | null | undefined): void {
+
+    if (name) {
+        console.log(`Hello ,${name.toUpperCase()}!!`)
+    } else {
+        console.log('Hello!!')
+    }
+}
+
+greet2(null);
+greet2(undefined);
+greet2('John');
+greet2(''); // empty string is treated as valid string
+
+//------------------------------
+//Optional Chaining and Nullish Coalescing
+
+type Customer = { birthday:Date };
+
+function getCustomer(id:number): Customer | null | undefined {
+
+    return id === 0? null : {birthday:new Date()};
+}
+
+let cust = getCustomer(0)
+if  (cust !== null && cust !== undefined){
+    console.log(cust.birthday);
+}
+
+//Using Optional Chaining: checks for null or undefined before accessing property
+console.log(cust?.birthday);
+console.log(cust?.birthday.getFullYear());
+//Using Nullish Coalescing: provides a default value if the left side is null or undefined
+
+let birthday = cust?.birthday.getFullYear() ?? 'No birthday available';
+console.log(birthday);
+
+//------------------------------
+//Type Assertions
+let phone = document.getElementById('phone') as HTMLInputElement;
+phone.value = '123-456-7890';
+
+//or
+let phone2 = <HTMLInputElement> document.getElementById('phone2');
+phone2.value = '987-654-3210';
+//------------------------------
+//Never Type
+function processEvents(): never {
+    while (true) {
+        // process events
+    }
+}
+//processEvents(); // Uncommenting this will cause an infinite loop
+//------------------------------
+//Type Inference
+let inferredString = 'This is a string'; // inferred as string
+//inferredString = 10; // Error
+console.log(inferredString);
+let inferredNumber = 20; // inferred as number
+//inferredNumber = 'Hello'; // Error
+console.log(inferredNumber);
+
+processEvents();
+//------------------------------
+
+
+
+
+
